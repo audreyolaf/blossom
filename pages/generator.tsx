77289c65837eebbd5bc115aab715d327fefda9a2
Audrey Wang <62197228+audreyolaf@users.noreply.github.com>
2022-08-03 21:26:09 +0000

    class -> className

--- pages/generator.tsx
+++ pages/generator.tsx
@@ -10,43 +10,43 @@ export default function Generator() {
   return (
     <div>
     <Navbar />
-      <div class="grid grid-flow-col grid-flow-col-dense">
-    <div class="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
+      <div className="grid grid-flow-col grid-flow-col-dense">
+    <div className="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">self love reminders</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">self love reminders</h5>
         </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{randomReminders[randomNumber]}</p>
-        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{randomReminders[randomNumber]}</p>
+        <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
             generate
         </a>
     </div>
 
-      <div class="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
+      <div className="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">daily journal prompts</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">daily journal prompts</h5>
         </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{randomPrompt[randomNumber]}</p>
-        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{randomPrompt[randomNumber]}</p>
+        <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
             generate
         </a>
     </div>
 
-    <div class="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
+    <div className="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">playlist</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">playlist</h5>
         </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">lofi</p>
-        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">lofi</p>
+        <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
             listen
         </a>
     </div>
 
-    <div class="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
+    <div className="m-8 p-6 max-w-sm bg-white rounded-lg bg-lightSal">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">playlist</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">playlist</h5>
         </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">collaborative</p>
-        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">collaborative</p>
+        <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-babyBlue rounded-lg">
             listen
         </a>
     </div>
--- pages/learn.tsx
+++ pages/learn.tsx
@@ -6,27 +6,27 @@ export default function Learn() {
     <div>
       <Navbar />
       <div className="bg-gradient-to-r from-[#FBB9C4] to-[#ECC8F0]">
-      <div className="bg-lightSal px-8 py-10 text-3xl font-bold text-center">
+      <div className="bg-lightSal px-8 py-8 text-3xl font-bold text-center">
           <h2 className="italic">every 52 minutes, at least one person dies as a result of an eating disorder.</h2>
       </div>
         
       <div className="m-50 p-5 text-center font-bold">
         <h2 className="text-3xl m-auto">here are the facts.</h2><br/>
         
-<div class="flex items-center">
-        <div class="flex flex-col px-10">
-           <span class="text-3xl px-5 m-auto bg-babyBlue rounded-lg">the impacts of social media</span><br/>
-            <p class="text-2xl">we all know that social media can be harmful, but just how much does it imapct teens? according to research conducted by xyz, it was found that xxx amount of teens are affected by zzzz this is important because of xxxx and we found that idk idk xyz researcher at university further found that disordered eating in girls xyz</p>
+<div className="flex items-center">
+        <div className="flex flex-col px-10">
+           <span className="text-3xl px-5 m-auto bg-babyBlue rounded-lg">the impacts of social media</span><br/>
+            <p className="text-2xl">we all know that social media can be harmful, but just how much does it imapct teens? according to research conducted by xyz, it was found that xxx amount of teens are affected by zzzz this is important because of xxxx and we found that idk idk xyz researcher at university further found that disordered eating in girls xyz</p>
         </div>
-        <img class="py-2 pr-4 ml-8 rounded-lg" src="https://static01.nyt.com/images/2022/07/05/science/30tb-panda/30tb-panda-articleLarge.jpg?quality=75&auto=webp&disable=upscale"></img>
+        <img className="py-2 pr-4 ml-8 rounded-lg" src="https://static01.nyt.com/images/2022/07/05/science/30tb-panda/30tb-panda-articleLarge.jpg?quality=75&auto=webp&disable=upscale"></img>
     </div>
 
-        <div class="flex items-center">
-        <div class="flex flex-col px-10">
-           <span class="text-3xl px-5 m-auto bg-babyBlue rounded-lg">fact check! </span><br/>
-            <p class="text-2xl">celebrities, influencers, peers, everyone edits their posts. but how often do they? and to what extent?<br />xyz professor researched xyz teens social media in 2018, finding that 90% of teens modify their body in social media posts. in popular platfroms such as instagram, facebook, tiktok, etc</p>
+        <div className="flex items-center">
+        <div className="flex flex-col px-10">
+           <span className="text-3xl px-5 m-auto bg-babyBlue rounded-lg">fact check! </span><br/>
+            <p className="text-2xl">celebrities, influencers, peers, everyone edits their posts. but how often do they? and to what extent?<br />xyz professor researched xyz teens social media in 2018, finding that 90% of teens modify their body in social media posts. in popular platfroms such as instagram, facebook, tiktok, etc</p>
         </div>
-        <img class="py-2 pr-4 ml-8 rounded-lg" src="https://static01.nyt.com/images/2022/07/05/science/30tb-panda/30tb-panda-articleLarge.jpg?quality=75&auto=webp&disable=upscale"></img>
+        <img className="py-2 pr-4 ml-8 rounded-lg" src="https://static01.nyt.com/images/2022/07/05/science/30tb-panda/30tb-panda-articleLarge.jpg?quality=75&auto=webp&disable=upscale"></img>
     </div>
         
         </div>
